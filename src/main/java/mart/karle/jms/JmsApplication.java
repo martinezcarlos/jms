e85001dfbc4e183fdfdcdb910f6325229b19da49
Carlos Martínez <ingeniero.carlos.martinez@gmail.com>
2019-11-18 11:40:14 +0100

    - ActiveMQ initial config to run in an embedded serve with basic configuration properties.

--- src/main/java/mart/karle/jms/JmsApplication.java
+++ src/main/java/mart/karle/jms/JmsApplication.java
@@ -1,12 +1,23 @@
 package mart.karle.jms;
 
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JmsApplication {
 
-  public static void main(String[] args) {
+  public static void main(final String[] args) throws Exception {
+    final ActiveMQServer activeMQServer =
+        ActiveMQServers.newActiveMQServer(
+            new ConfigurationImpl()
+                .setPersistenceEnabled(false)
+                .setJournalDirectory("target/data/journal")
+                .setSecurityEnabled(false)
+                .addAcceptorConfiguration("invm", "vm://0"));
+    activeMQServer.start();
     SpringApplication.run(JmsApplication.class, args);
   }
 }
---
+++ src/main/java/mart/karle/jms/model/Message.java
@@ -0,0 +1,19 @@
+package mart.karle.jms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message implements Serializable {
+  private static final long serialVersionUID = -4670559120294333686L;
+  private UUID id;
+  private String message;
+}
