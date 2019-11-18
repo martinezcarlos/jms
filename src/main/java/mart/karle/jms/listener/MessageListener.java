2512d601b67ed39a33f0b98bf12c764e96564c33
Carlos Martínez <ingeniero.carlos.martinez@gmail.com>
2019-11-18 14:56:03 +0100

    - Modified to send and receive a message.

--- src/main/java/mart/karle/jms/listener/MessageListener.java
+++ src/main/java/mart/karle/jms/listener/MessageListener.java
@@ -1,23 +1,42 @@
 package mart.karle.jms.listener;
 
+import lombok.RequiredArgsConstructor;
 import mart.karle.jms.config.JmsConfig;
 import mart.karle.jms.model.HelloMessage;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
 import javax.jms.Message;
+import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor
 public class MessageListener {
+
+  private final JmsTemplate jmsTemplate;
+
   @JmsListener(destination = JmsConfig.MY_QUEUE)
   public void listen(
       @Payload final HelloMessage helloMessage,
       @Headers final MessageHeaders headers,
       final Message message) {
-    System.out.println("Got message...");
-    System.out.println(helloMessage);
+    //    System.out.println("Got message...");
+    //    System.out.println(helloMessage);
+  }
+
+  @JmsListener(destination = JmsConfig.MY_SEND_RECEIVE_QUEUE)
+  public void listenForHello(
+      @Payload final HelloMessage helloMessage,
+      @Headers final MessageHeaders headers,
+      final Message message)
+      throws JMSException {
+    final HelloMessage replyMessage =
+        HelloMessage.builder().id(UUID.randomUUID()).message("World!").build();
+    jmsTemplate.convertAndSend(message.getJMSReplyTo(), replyMessage);
   }
 }
--- src/main/java/mart/karle/jms/sender/Sender.java
+++ src/main/java/mart/karle/jms/sender/Sender.java
@@ -1,5 +1,7 @@
 package mart.karle.jms.sender;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import mart.karle.jms.config.JmsConfig;
 import mart.karle.jms.model.HelloMessage;
@@ -7,6 +9,8 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
 import java.util.UUID;
 
 @Component
@@ -14,13 +18,37 @@ import java.util.UUID;
 public class Sender {
 
   private final JmsTemplate jmsTemplate;
+  private final ObjectMapper objectMapper;
 
   @Scheduled(fixedRate = 2000L)
   public void sendMessage() {
-    System.out.println("Sending message...");
+    //    System.out.println("Sending message...");
     final HelloMessage helloMessage =
         HelloMessage.builder().id(UUID.randomUUID()).message("Hello world!").build();
     jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, helloMessage);
-    System.out.println("Message sent!");
+    //    System.out.println("Message sent!");
+  }
+
+  @Scheduled(fixedRate = 2000L)
+  public void sendAndReceiveMessage() throws JMSException {
+    System.out.println("Sending message...");
+    final HelloMessage helloMessage =
+        HelloMessage.builder().id(UUID.randomUUID()).message("Hello").build();
+    final Message received =
+        jmsTemplate.sendAndReceive(
+            JmsConfig.MY_SEND_RECEIVE_QUEUE,
+            session -> {
+              final Message textMessage;
+              try {
+                textMessage =
+                    session.createTextMessage(objectMapper.writeValueAsString(helloMessage));
+                textMessage.setStringProperty("_type", HelloMessage.class.getCanonicalName());
+                System.out.println("Sending hello...");
+                return textMessage;
+              } catch (final JsonProcessingException e) {
+                throw new JMSException("Failed");
+              }
+            });
+    System.out.println(received.getBody(String.class));
   }
 }
