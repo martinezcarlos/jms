1821d9efec9415e3628f9fbe9e3ccebd07daa829
Carlos Martínez <ingeniero.carlos.martinez@gmail.com>
2019-11-18 11:25:25 +0100

    Initial commit

---
+++ src/test/java/mart/karle/jms/JmsApplicationTests.java
@@ -0,0 +1,11 @@
+package mart.karle.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
